5ff7272d695c40352e6ade11c8d5be93b7b7ea27
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-12-19 03:57:01 +0530

    used async/await instead of promises syntax

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ const img = document.querySelector("img")
 let query;
 
 
-function getGif(query){
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UEHN36ASRiIUGyxW3F2iaLPdY23oRAkq&s=${query}`,{mode:"cors"})
-    .then(function(response){
-        img.src = "./images/loading.gif"
-        console.log("image loading");
-        return response.json();
-    })
-    .then(function(response){
-        console.log(response);
-        img.src = response.data.images.original.url
-        console.log("image loaded");
+async function getGif(query){
+
+    img.src = "./images/loading.gif"
+    console.log("Image Loading");
+    const rawResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UEHN36ASRiIUGyxW3F2iaLPdY23oRAkq&s=${query}`,{mode:"cors"})
+    const JSONresponse = await rawResponse.json();
+    img.src = JSONresponse.data.images.original.url
+    console.log("Image Loaded");
+
+
+    // fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UEHN36ASRiIUGyxW3F2iaLPdY23oRAkq&s=${query}`,{mode:"cors"})
+    // .then(function(response){
+    //     img.src = "./images/loading.gif"
+    //     console.log("image loading");
+    //     return response.json();
+    // })
+    // .then(function(response){
+    //     console.log(response);
+    //     img.src = response.data.images.original.url
+    //     console.log("image loaded");
         
-    })
+    // })
 }
 
 document.addEventListener('keypress',(event)=>{
