a5990c7d418c59f88a5274ed7e4cc49d0f8bbb32
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-12-19 00:21:21 +0530

    posting to git

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputBox = document.querySelector("#inputBox")
+const form = document.querySelector("form")
+const img = document.querySelector("img")
+// const loading = "./images/loading.gif"
+let query;
+
+
+function getGif(query){
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UEHN36ASRiIUGyxW3F2iaLPdY23oRAkq&s=${query}`,{mode:"cors"})
+    .then(function(response){
+        img.src = "./images/loading.gif"
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response);
+        img.src = response.data.images.original.url
+    })
+}
+
+document.addEventListener('keypress',(event)=>{
+    if(event.key == "Enter"){
+        event.preventDefault();
+        let text = inputBox.value;
+        query = text.replaceAll(" ","+")
+        console.log(query);
+
+        getGif(query);
+    }
+})
+
